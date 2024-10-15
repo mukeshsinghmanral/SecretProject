5e1ad592173093b822d983801e9eade8a26e6981
Mukesh Singh Manral <69538555+mukeshsinghmanral@users.noreply.github.com>
2024-10-16 04:01:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets",
+  callbackURL: "https://secretproject-a18b.onrender.com/auth/google/secrets",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
 async (accessToken, refreshToken, profile, cb) => {
